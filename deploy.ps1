f4019202770552d4e176612dd7649eade11f098a
Gospodin Dito <dominik.dito@gmail.com>
2020-08-04 00:42:38 +0200

    deploy update

--- deploy.ps1
+++ deploy.ps1
@@ -2,6 +2,7 @@ if (Test-Path .\docs\) { Remove-Item -Recurse .\docs\ }
 cd .\src\whichmovietowatch/
 ng build --prod --deploy-url="whichmovietowatch/"
 cd ..\..
-Robocopy.exe .\src\whichmovietowatch\dist .\docs
+Robocopy.exe .\src\whichmovietowatch\dist\whichmovietowatch .\docs /MIR
+Robocopy.exe .\src\whichmovietowatch\dist\whichmovietowatch .\docs\whichmovietowatch /MIR
 git add --all .
 git push --force
